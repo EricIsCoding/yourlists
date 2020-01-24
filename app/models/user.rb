6bcc3e30570c006d3b149b9b6ae3b001a85d7c68
ericiscoding <ericjwilhite@gmail.com>
2020-01-24 13:07:27 -0800

    Add basic validations to User attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-    
+    validates :username, presence: true, uniqeness: true, length: {minimum: 5}
+    validates :name, presence: true, uniqeness: true
+    validates :email, presence: true, uniqeness: true
 end
