5b67a98175b4d9a3ea34c481f23ab6e2f9d7f84d
ericiscoding <ericjwilhite@gmail.com>
2020-01-24 16:54:06 -0800

    Add Devise Gem and Update Default Sign-up View

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqeness: true, length: {minimum: 5}
-    validates :name, presence: true, uniqeness: true
-    validates :email, presence: true, uniqeness: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+    validates :username, presence: true, uniqueness: true, length: {minimum: 5}
+    validates :name, presence: true, uniqueness: true
 end
