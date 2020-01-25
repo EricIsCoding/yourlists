ab68e595d1c8c2049f9544938f8d4a20ddf6d3f9
Eric J Wilhite <EricJWilhite@gmail.com>
2020-01-25 14:42:35 -0800

    Add New and Create Actions to ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,16 @@ class ItemsController < ApplicationController
     before_action :authenticate_user!
 
     def new
-        
+        @item = Item.new
     end
 
     def create
-
+        @item = Item.create(items_params)
+        if @item.persisted?
+            redirect_to user_path(current_user)
+        else 
+            render :new
+        end
     end
 
     def edit
@@ -24,7 +29,7 @@ class ItemsController < ApplicationController
     private
 
     def items_params
-        params.require(:items).permit(:name, :description)
+        params.require(:item).permit(:name, :description, :section_id)
     end
 
 end
